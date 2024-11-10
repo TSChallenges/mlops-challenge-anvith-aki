bfc2f7382e1c69683ff843bda4f8a3a48142fa65
anvith-aki <75977318+anvith-aki@users.noreply.github.com>
2024-11-10 12:18:24 +0000

    test 1

--- src/run_model.py
+++ src/run_model.py
@@ -9,15 +9,22 @@ sys.path.append(str(root))
 
 def load_model():
     
-    model_path = '/models/churn_model.pkl' 
+    # model_path = '/models/churn_model.pkl' 
+    model_path = '/workspaces/mlops-challenge-anvith-aki/models/churn_model.pkl' 
     with open(model_path, 'rb') as model_file:
         model = pickle.load(model_file)
     return model
 
 def get_customer_input():
     credit_score = float(input("Enter credit score: "))
-    # ENTER YOUR CODE HERE
-    return [[credit_score]]
+    age = int(input("Enter age: "))
+    balance = float(input("Enter balance: "))
+    num_of_products = int(input("num_of_products: "))
+    input5 = int(input("input: "))
+    input6 = int(input("input: "))
+    input7 = int(input("input: "))
+    input8 = int(input("input: "))
+    return [[credit_score, age, balance, num_of_products, input5, input6, input7, input8]]
 
 def predict_churn(model, customer_data):
     prediction = model.predict(customer_data)
